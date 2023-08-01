cb9452e77eb99d5334311f258a25226d1dd85afd
Никита Сухоруков <mrHarker33@gmail.com>
2023-08-01 15:49:49 +0300

    add docstr

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from random import randint
-from graphic_arts.start_game_banner import run_screensaver
+#from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Attack calculation."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -16,6 +17,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Defence calculation."""
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
@@ -26,6 +28,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Calculate ability."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -37,6 +40,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Training."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -61,6 +65,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Class choosing."""
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -82,8 +87,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
-    run_screensaver()
+if __name__ == '__main__':
+    #run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -95,4 +100,4 @@ def main() -> None:
     print(start_training(char_name, char_class))
 
 
-main()
+
